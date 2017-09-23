e182107d03972b33241ae714233aaf5bb4a905cd
Andres Valdovinos <valdovinosandres@Andress-MacBook-Pro.local>
2017-09-23 15:50:36 -0700

    Added stuff

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+  connection =mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
