28279bb17a5e5d14c78975ab6702142cad2d1325
Andres Valdovinos <avaldov1@uci.edu>
2017-09-24 14:44:04 -0700

    almost complete

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,16 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} 
+} else{
   connection =mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgers_db"
-});
-
+  });
+}
+console.log("CHECK");
 // Make connection.
 connection.connect(function(err) {
   if (err) {
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,3 +4,33 @@ var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
+
+
+router.get("/", function(req,res){
+	burger.selectAll(function(data){
+		console.log(data);
+		res.render("index", {burgers:data});
+	});
+});
+
+
+router.post("/", function(req,res){
+	console.log(req.body);
+	burger.insertOne(["burger_name", "devoured"], [req.body.name, false], function(){
+		res.redirect("/");
+	});
+});
+
+router.put("/:id", function(req,res){
+	var condition = "id = " + req.params.id;
+
+	console.log("condition: ", condition);
+
+	burger.updateOne({
+		devoured: true
+	}, condition, function(){
+		res.redirect("/");
+	});
+});
+
+module.exports = router;
